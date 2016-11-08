26bbd925a9de2d0ec49a594d53af5f810e65dd4e
Dudarev Ilia <dudareviv@gmail.com>
2016-11-08 13:52:33 +0400

    Измнил окружение. Включил ЧПУ.

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=nirs',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
